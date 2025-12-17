d90c1508379cb945f0878ec6fe9ecbd8d4a299e7
Valeza Dobruna <valezadobruna@gmail.com>
2025-12-18 00:17:16 +0100

    Komunikimi mes frontend dhe backend

--- KinoTimeBackEnd/Program.cs
+++ KinoTimeBackEnd/Program.cs
@@ -1,18 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Add services to the container
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowReactApp");
+
+app.MapControllers();
 
 var summaries = new[]
 {
@@ -21,7 +36,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- kinotimefrontend/src/App.js
+++ kinotimefrontend/src/App.js
@@ -1,14 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5296/weatherforecast") // kontrollo portin e backend
+      .then(response => response.json())
+      .then(data => setWeather(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello from kinotimefrontend!
-        </p>
+        <p>hello from kinotimefrontend!</p>
+
+        <ul>
+          {weather.map((w, index) => (
+            <li key={index}>
+              {w.date} - {w.temperatureC}°C - {w.summary}
+            </li>
+          ))}
+        </ul>
+
         <a
           className="App-link"
           href="https://reactjs.org"
